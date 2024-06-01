4f94d0e0e20107d5c88298dd4430c707a4972cf5
Avnish Patel <avnishpatel.ce@hotmail.com>
2024-06-01 15:32:37 +0530

    Add project files.

---
+++ AttributeValidation/UniqueEmailAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebAppCustomDataAnnotation.ArrtibuteValidation
+{
+
+    public class UniqueEmailAttribute : ValidationAttribute
+    {
+
+        protected override ValidationResult IsValid(object _value, ValidationContext validationContext)
+        {
+
+            if (_value != null && _value.ToString() == "name@company.com")
+            {
+                return new ValidationResult(ErrorMessage = "Email address already exist");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebAppCustomDataAnnotation.ArrtibuteValidation;
+
+namespace WebAppCustomDataAnnotation.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Email address is required.")]
+        [UniqueEmail]
+        public string Email { get; set; }
+    }
+}
